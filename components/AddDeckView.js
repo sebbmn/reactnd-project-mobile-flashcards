82710e47c6b6b20bb3a4116b034b537a5e2912ed
sebbmn <sebbmn@gmail.com>
2017-10-20 21:20:38 +0200

    feat: Layout and style of AddDeckView

--- components/AddDeckView.js
+++ components/AddDeckView.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { Text, View, TextInput, Button } from 'react-native'
+import { Text, View, KeyboardAvoidingView, TextInput, TouchableOpacity, StyleSheet } from 'react-native'
 
 export default class AddDeckView extends Component {
   state = {
@@ -13,20 +13,52 @@ export default class AddDeckView extends Component {
   }
   render() {
     return (
-      <View>
-        <Text style={{fontSize:30, margin:20}}>
+      <View style={styles.container}>
+        <Text style={styles.title}>
           What is the title of your new deck?
         </Text>
-        <TextInput
-          value={this.state.deckTitle}
-          onChangeText={(text) => this.setState({deckTitle:text})}
-        />
-        <Button
-          onPress={this.submit}
-          title="Submit"
-          color="#000000"
-        />
+        <View>
+          <TextInput
+            value={this.state.deckTitle}
+            onChangeText={(text) => this.setState({deckTitle:text})}
+            style={styles.textinput}
+          />
+        </View>
+        <View style={{alignSelf: 'center'}}>
+          <TouchableOpacity onPress={this.submit}>
+            <Text style={styles.buttonSubmit}>Submit</Text>
+          </TouchableOpacity>
+        </View>
       </View>
     )
   }
 }
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'space-around',
+    margin: 40,
+  },
+  title: {
+    fontSize: 40,
+    textAlign: 'center'
+  },
+  textinput: {
+    borderWidth: 1,
+    borderColor: '#000000',
+    borderRadius: 5,
+    padding: 5,
+  },
+  buttonSubmit: {
+    backgroundColor: '#000000',
+    color: '#ffffff',
+    justifyContent:'center',
+    width: 150,
+    height: 50,
+    borderRadius: 5,
+    fontSize: 20,
+    textAlign: 'center',
+    padding: 10,
+    margin: 5
+  },
+})
