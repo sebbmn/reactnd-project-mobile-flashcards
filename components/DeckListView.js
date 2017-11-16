6777f8991252b44958cdd75243d10b3f2b097d5b
sebbmn <sebbmn@gmail.com>
2017-11-16 20:47:42 +0100

    refactor: remove test functions in DeckListView

--- components/DeckListView.js
+++ components/DeckListView.js
@@ -4,61 +4,23 @@ import { addDeck, addCard } from '../actions'
 import { Text, View, Button, TouchableOpacity, StyleSheet, FlatList } from 'react-native'
 import {saveDeckTitle, getDecks, clearAll, getDeck, addCardToDeck} from '../utils/api'
 
-function populate() {
-  const initialDeckList = {
-    React: {
-      title: 'React',
-      questions: [
-        {
-          question: 'What is React??',
-          answer: 'A library for managing user interfaces'
-        },
-        {
-          question: 'Where do you make Ajax requests in React?',
-          answer: 'The componentDidMount lifecycle event'
-        }
-      ]
-    },
-    JavaScript: {
-      title: 'JavaScript',
-      questions: [
-        {
-          question: 'What is a closure?',
-          answer: 'The combination of a function and the lexical environment within which that function was declared.'
-        }
-      ]
-    }
-  }
-  //clearAll()
-  saveDeckTitle('React')
-  /**saveDeckTitle('JavaScript') 
-
-  addCardToDeck('JavaScript', initialDeckList.JavaScript.questions[0])   
-  **/
-  return addCardToDeck('React', initialDeckList.React.questions[0]).then(() => {
-    addCardToDeck('React', initialDeckList.React.questions[1])
-  })
-}
-
 class DeckListView extends Component {
   componentDidMount = () => {
     //clearAll()
-     //populate().then(() => {
-      getDecks().then( (results) => {
-        results.map( (deckName) => {
-          if(deckName !== 'MobileFlashCards:notifications') {
-            this.props.addD(deckName)
-            getDeck(deckName).then((deck) => {
-              JSON.parse(deck).questions.map ( (card) => {
-                this.props.addC({deckName: deckName ,question: card.question, answer: card.answer})
-              })
-          })} else {
-            getDeck(deckName).then((deck) => {
+    getDecks().then( (results) => {
+      results.map( (deckName) => {
+        if(deckName !== 'MobileFlashCards:notifications') {
+          this.props.addD(deckName)
+          getDeck(deckName).then((deck) => {
+            JSON.parse(deck).questions.map ( (card) => {
+              this.props.addC({deckName: deckName ,question: card.question, answer: card.answer})
             })
-          }
-        })
+        })} else {
+          getDeck(deckName).then((deck) => {
+          })
+        }
       })
-    //})
+    })
   }
   render() {
     const { deckList } = this.props
